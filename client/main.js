d232b8becd2528428ff00c4b8e0a86d1eff41806
ms-saep <michele.strambi@saep-ict.it>
2019-10-02 15:15:46 +0200

    testFunction

--- client/main.js
+++ client/main.js
@@ -1,12 +1,17 @@
-const io = require('socket.io');
+// const io = require('socket.io');
 
 function createRoom() {
-   let socket = io('/rooms');
-   socket.on('connect', () => {
-       console.log(socket.id);
-   });
+    console.log("PDio")
+   // let socket = io('/rooms');
+   // socket.on('connect', () => {
+   //     console.log(socket.id);
+   // });
+   //
+   // socket.on('my message', function (msg) {
+   //     console.log(msg)
+   // })
+}
 
-   socket.on('my message', function (msg) {
-       console.log(msg)
-   })
+export function testFunction() {
+    console.log(`[[ testFunction ]] called`);
 }
