fc2ed503f74a437c8eae9f63051a30e77ffdb937
mich <michele.strambi@gmail.com>
2019-10-02 22:30:01 +0200

    working socket client

--- client/main.js
+++ client/main.js
@@ -1,21 +1,22 @@
-// const io = require('socket.io');
+var io = require('socket.io-client');
 const main = require('./myFunctions');
 const $ = require('jquery/dist/jquery');
 
 // main.foo();
 
 $('#my-button').click(testFunction);
+$('#create-room-button').click(createRoom);
 
 function createRoom() {
-    console.log("PDio")
-   // let socket = io('/rooms');
-   // socket.on('connect', () => {
-   //     console.log(socket.id);
-   // });
-   //
-   // socket.on('my message', function (msg) {
-   //     console.log(msg)
-   // })
+   console.log("[[ createRoom ]] called");
+   let socket = io('http://localhost:3000');
+   socket.on('connect', () => {
+       console.log(socket.id);
+   });
+
+   socket.on('my message', function (msg) {
+       console.log(msg)
+   })
 }
 
 function testFunction() {
